d094b7cbf2666c66abb9260360ab54e3836bc84d
Vincent <vincent.pham2@outlook.com>
2024-07-15 08:21:44 +1000

    Upload model evaluation results with additional models

---
+++ models/vgg16.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torchvision.models as models
+
+class Vgg16(nn.Module):
+    def __init__(self):
+        super().__init__()
+        weights = models.VGG16_BN_Weights.DEFAULT
+        self.model = models.vgg16_bn(weights=weights)
+        n_features = self.model.classifier[6].in_features
+        self.model.classifier[6] = nn.Linear(n_features, 7)
+
+    def forward(self, x):
+        x = x.repeat(1, 3, 1, 1)
+        x = self.model(x)
+        return x
