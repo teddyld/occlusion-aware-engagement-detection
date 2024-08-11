d0ebf07168e6873be10ebbfd9ee10611179f5331
Vincent <vincent.pham2@outlook.com>
2024-08-12 00:24:30 +1000

    Update config with DAiSEE paths

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 import os
 
+# FER-2013 Paths
 DATA_PATH = os.path.abspath('./data/FER-2013')
 
 CSV_PATH = DATA_PATH + '/icml_face_data.csv'
@@ -8,4 +9,13 @@ VAL_PATH = DATA_PATH + '/val'
 TEST_PATH = DATA_PATH + '/test'
 ANNOTATIONS_PATH = DATA_PATH + '/annotations'
 
+# ALOT Path
 ALOT_DATA_PATH = os.path.abspath('./data/ALOT')
+
+# DAiSEE Paths
+DAISEE_DATA_PATH = os.path.abspath('./data/DAiSEE/DataSet')
+
+DAISEE_TRAIN_PATH = DAISEE_DATA_PATH + '/Train'
+DAISEE_VAL_PATH = DAISEE_DATA_PATH + '/Validation'
+DAISEE_TEST_PATH = DAISEE_DATA_PATH + '/Test'
+DAISEE_ANNOTATIONS_PATH = DAISEE_DATA_PATH + '/../Labels'
